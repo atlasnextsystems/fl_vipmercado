9ed5966d0f6754e11d542e697be8feff4934ceaa
Allan Matias <alangiovanepaes@gmail.com>
2026-07-18 16:02:45 -0300

    multi units

--- src/data/stores.ts
+++ src/data/stores.ts
@@ -18,7 +18,7 @@ export const STORES: Store[] = [
     address: 'Avenida Anita Garibaldi 240, Salto, São Paulo',
     cep: 'CEP 13323-570',
     directionsUrl: 'https://www.google.com/maps/dir/?api=1&destination=Avenida+Anita+Garibaldi+240,+Salto,+SP',
-    embedMapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3667.6534570077977!2d-47.2847942!3d-23.2010839!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c5f5904ffbc37b%3A0xc0f1b20127167201!2sAv.%20Anita%20Garibaldi%2C%20240%20-%20Salto%2C%20SP%2C%2013323-570!5e0!3m2!1spt-BR!2sbr!4v1784396860000',
+    embedMapUrl: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1833.2708494691171!2d-47.265783794350284!3d-23.223367011632167!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf4f0a1925c28b%3A0x80b7389c8481b715!2sSupermercado%20Vip!5e0!3m2!1spt-BR!2sbr!4v1784401296608!5m2!1spt-BR!2sbr',
     phone: '(11) 97167-2012',
     hours: 'Segunda a Sábado: 08:30 às 19:00 | Domingo: Fechado'
   },
